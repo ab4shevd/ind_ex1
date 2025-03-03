77f3941fb62827144ab77b72c0cafa503b93285f
ab4shevd <abashev1999gggg@gmail.com>
2025-03-03 09:39:20 +0300

    Testing the function

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import random
 def generate_random_list():
     random_list = [random.randint(5, 100) for _ in range(11)]
     return random_list
+
+random_list = generate_random_list()
+print(random_list)
+print("The program is finished")
