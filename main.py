2e4f3a0997d2ad82e6f2fdf2c35b0784cbc9576b
ab4shevd <abashev1999gggg@gmail.com>
2025-03-03 09:41:08 +0300

    Updating the contents of the gitignore file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def generate_random_list():
     random_list = [random.randint(5, 100) for _ in range(11)]
     return random_list
 
+# Тестирование функциии и добавление gitignore
 random_list = generate_random_list()
 print(random_list)
 print("The program is finished")
